fb40347ed37bbc1821a261800225754c33f2f361
Jeremias Yehdegho <j.yehdegho+github@gmail.com>
2019-04-03 12:29:11 +0200

    show boundingbox, use subimage

--- app/src/main/java/com/amitshekhar/tflite/MainActivity.java
+++ app/src/main/java/com/amitshekhar/tflite/MainActivity.java
@@ -1,13 +1,25 @@
 package com.amitshekhar.tflite;
 
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.wonderkiln.camerakit.CameraKit;
 import com.wonderkiln.camerakit.CameraKitError;
@@ -20,13 +32,15 @@ import com.wonderkiln.camerakit.CameraView;
 import org.christopherfrantz.dbscan.DBSCANClusterer;
 import org.christopherfrantz.dbscan.DBSCANClusteringException;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int PERMISSION_REQUEST_CODE = 1;
     private static final String MODEL_PATH = "facenet.tflite";
     private static final boolean QUANT = true;
     private static final int INPUT_SIZE = 160;
@@ -34,10 +48,39 @@ public class MainActivity extends AppCompatActivity {
     private Classifier classifier;
 
     private Executor executor = Executors.newSingleThreadExecutor();
-    //private TextView textViewResult;
-    //private Button btnDetectObject, btnToggleCamera;
-    //private ImageView imageViewResult;
     private CameraView cameraView;
+    private FloatingActionButton fab;
+    private ShapeDrawable boundingBox;
+    private View boundingBoxView;
+
+    private void writeImg(Bitmap bitmap) {
+        File applicationDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS); //getApplication().getFilesDir();
+
+        File outFile = new File(applicationDir, "bla.jpg");
+
+        Log.d("fuck", outFile.toString());
+
+        try (FileOutputStream out = new FileOutputStream(outFile)) {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public  boolean requestRuntimePermissions() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+            }
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,14 +88,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         cameraView = findViewById(R.id.cameraView);
         cameraView.setFacing(CameraKit.Constants.FACING_FRONT);
+        fab = findViewById(R.id.fab);
 
-        /*imageViewResult = findViewById(R.id.imageViewResult);
-        textViewResult = findViewById(R.id.textViewResult);
-        textViewResult.setMovementMethod(new ScrollingMovementMethod());
+        requestRuntimePermissions();
 
-        btnToggleCamera = findViewById(R.id.btnToggleCamera);
-        btnDetectObject = findViewById(R.id.btnDetectObject);
-*/
         cameraView.addCameraKitListener(new CameraKitEventListener() {
             @Override
             public void onEvent(CameraKitEvent cameraKitEvent) {
@@ -66,11 +105,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onImage(CameraKitImage cameraKitImage) {
-
                 Bitmap bitmap = cameraKitImage.getBitmap();
 
+                bitmap = Bitmap.createBitmap(bitmap, 300, 1100, 1400, 1400);
                 bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
 
+                //writeImg(bitmap);
                 //imageViewResult.setImageBitmap(bitmap);
 
                 final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
@@ -103,19 +143,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /*btnToggleCamera.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                cameraView.toggleFacing();
-            }
-        });
-
-        btnDetectObject.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 cameraView.captureImage();
             }
-        });*/
+        });
 
         initTensorFlowAndLoadModel();
     }
