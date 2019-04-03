0582d1db924dbab4bf8fa50604f87237dcf1fda3
Jeremias Yehdegho <j.yehdegho+github@gmail.com>
2019-04-03 09:17:26 +0200

    dead end, doesnt work

--- app/src/main/java/com/amitshekhar/tflite/MainActivity.java
+++ app/src/main/java/com/amitshekhar/tflite/MainActivity.java
@@ -1,24 +1,9 @@
 package com.amitshekhar.tflite;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.camerakit.CameraKitView;
-
-import org.christopherfrantz.dbscan.DBSCANClusterer;
-import org.christopherfrantz.dbscan.DBSCANClusteringException;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,130 +11,36 @@ public class MainActivity extends AppCompatActivity {
     private static final boolean QUANT = true;
     private static final int INPUT_SIZE = 160;
 
-    private Classifier classifier;
-
-    private Executor executor = Executors.newSingleThreadExecutor();
-    private TextView textViewResult;
-    private Button btnDetectObject, btnToggleCamera;
-    private ImageView imageViewResult;
-    private CameraKitView cameraView;
+    private CameraKitView cameraKitView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cameraView = findViewById(R.id.cameraView);
-        imageViewResult = findViewById(R.id.imageViewResult);
-        textViewResult = findViewById(R.id.textViewResult);
-        textViewResult.setMovementMethod(new ScrollingMovementMethod());
-
-        btnToggleCamera = findViewById(R.id.btnToggleCamera);
-        btnDetectObject = findViewById(R.id.btnDetectObject);
-
-
-
-        btnToggleCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                cameraView.captureImage(new CameraKitView.ImageCallback() {
-                    @Override
-                    public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
-                        Bitmap bitmap = BitmapFactory.decodeByteArray(capturedImage, 0, capturedImage.length);
-                        bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
-
-                        imageViewResult.setImageBitmap(bitmap);
-
-                        final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
-
-
-                        DBSCANClusterer<Classifier.Recognition> clusterer = null;
-                        List<ArrayList<Classifier.Recognition>> clusterResults = null;
-                        try {
-                            clusterer = new DBSCANClusterer<>(results, 2, 1.2, new Classifier.DistanceMetricRecognition());
-                            clusterResults = clusterer.performClustering();
-
-
-                            StringBuilder sb = new StringBuilder();
-                            sb.append("Number of pictures taken = ");
-                            sb.append(results.size());
-                            sb.append("\n");
-                            sb.append("Number of clusters = ");
-                            sb.append(clusterResults.size());
-
-                            textViewResult.setText(sb.toString());
-
-                        } catch (DBSCANClusteringException e) {
-                            textViewResult.setText(e.toString());
-                        }
-                    }
-                });
-
-            }
-        });
-
-        initTensorFlowAndLoadModel();
+        cameraKitView = findViewById(R.id.cameraView);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        cameraView.onResume();
+        cameraKitView.onResume();
     }
 
     @Override
     protected void onPause() {
-        cameraView.onPause();
+        cameraKitView.onPause();
         super.onPause();
     }
 
     @Override
     protected void onStop() {
-        cameraView.onStop();
+        cameraKitView.onStop();
         super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        executor.execute(new Runnable() {
-            @Override
-            public void run() {
-                classifier.close();
-            }
-        });
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        cameraView.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    private void initTensorFlowAndLoadModel() {
-        executor.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    classifier = TensorFlowImageClassifier.create(
-                            getAssets(),
-                            MODEL_PATH,
-                            INPUT_SIZE,
-                            QUANT);
-                    makeButtonVisible();
-                } catch (final Exception e) {
-                    throw new RuntimeException("Error initializing TensorFlow!", e);
-                }
-            }
-        });
-    }
-
-    private void makeButtonVisible() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                btnDetectObject.setVisibility(View.VISIBLE);
-            }
-        });
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
